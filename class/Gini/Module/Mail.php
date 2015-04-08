0d051c970df927bfa2c50ed8f415cfde9d772c87
pihizi <pihizi@msn.com>
2015-04-08 14:00:50 +0800

    增加默认发件人的配置检测

---
+++ class/Gini/Module/Mail.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Gini\Module;
+
+class Mail
+{
+    public static function diagnose()
+    {
+        // 建议设置默认的邮件发送人
+        // 因为mail提供了这个功能
+        $sender = (object) Config::get('system.postmaster');
+        if (!$sender->email || !$sender.name) {
+            return ['Please config your default mail sender (postmaster.email & postmaster.name) in system.yml!'];
+        }
+    }
+}
+
