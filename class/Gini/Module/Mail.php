778e411044761f9b35a6476aabb8782dc11eb404
pihizi <pihizi@msn.com>
2015-04-13 16:01:33 +0800

    javascript和php语言切换的不好

--- class/Gini/Module/Mail.php
+++ class/Gini/Module/Mail.php
@@ -9,7 +9,7 @@ class Mail
         // 建议设置默认的邮件发送人
         // 因为mail提供了这个功能
         $sender = (object) \Gini\Config::get('system.postmaster');
-        if (!$sender->email || !$sender.name) {
+        if (!$sender->email || !$sender->name) {
             return ['Please config your default mail sender (postmaster.email & postmaster.name) in system.yml!'];
         }
     }
