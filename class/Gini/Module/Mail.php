be6af03f204778978654c9a7bef57550dcb809ce
pihizi <pihizi@msn.com>
2015-04-13 15:35:26 +0800

    Config => \Gini\Config

--- class/Gini/Module/Mail.php
+++ class/Gini/Module/Mail.php
@@ -8,7 +8,7 @@ class Mail
     {
         // 建议设置默认的邮件发送人
         // 因为mail提供了这个功能
-        $sender = (object) Config::get('system.postmaster');
+        $sender = (object) \Gini\Config::get('system.postmaster');
         if (!$sender->email || !$sender.name) {
             return ['Please config your default mail sender (postmaster.email & postmaster.name) in system.yml!'];
         }
